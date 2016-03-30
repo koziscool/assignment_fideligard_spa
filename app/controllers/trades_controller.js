081bc6a207ec6fafaa70e16c1c6e2b25a7ae3ae5
koziscool <kozjob@yahoo.com>
2016-03-30 18:58:07 -0500

    stuff

--- app/controllers/trades_controller.js
+++ app/controllers/trades_controller.js
@@ -1,7 +1,16 @@
-stocks.controller('TradeCtrl', ['$scope', '$stateParams', function($scope, $stateParams) {
 
-  // console.log($('#dateSlider').val());
+stocks.controller('TradeCtrl', ['$scope', '$stateParams', 'PortfolioService', function($scope, $stateParams, 'PortfolioService') {
+
   console.log($stateParams);
+
+  $scope.formData = {
+    symbol: 'CSCO',
+    direction: "Buy",
+    price: 33.22,
+    quantity: 100,
+    date: new Date(),
+  };
+
 
   $scope.handleForm = function(){
     console.log('tetris');
